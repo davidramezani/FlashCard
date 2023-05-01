1b8d51ac3145ffc69665d9ac8531075925bab458
davoud.ramezani <dav.ramezani@gmail.com>
2023-05-01 13:39:52 +0330

    defined foreign key

--- app/src/main/java/com/davidramezani/davidflashcard/data/local/database/entities/CategoryEntity.kt
+++ app/src/main/java/com/davidramezani/davidflashcard/data/local/database/entities/CategoryEntity.kt
@@ -1,8 +1,19 @@
 package com.davidramezani.davidflashcard.data.local.database.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
-@Entity(tableName = "category")
+@Entity(
+    tableName = "category",
+    foreignKeys = [
+        ForeignKey(
+            entity = WordEntity::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("category_id"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class CategoryEntity(
     val id : Int,
     val title : String,
